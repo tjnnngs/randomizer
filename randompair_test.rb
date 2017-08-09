045a9a64224dfdf6753c2546b54763ce47e67bae
TONYA JENNINGS <you@example.com>
2017-08-08 23:28:44 -0400

    random pair array

--- randompair_test.rb
+++ randompair_test.rb
@@ -6,5 +6,8 @@ class TestRandompair < Minitest::Test
 	def test_assert_that_1_equals_1
 		assert_equal(1, 1)
 	end
-end
 
+	def test_assert_that_there_is_an_array
+		assert_equal([],student_list())
+	end
+end
