6b24d7bed57179ae4de65d0f60d795965db2c591
TONYA JENNINGS <you@example.com>
2017-08-10 21:57:24 -0400

    fixed for now

--- randompair.rb
+++ randompair.rb
@@ -1,11 +1,8 @@
 def student_list(student_name)
 	student_array = []
-	if student_name = ""
-		student_array
-	else
+	if student_name != ""
 		student_array.push(student_name)
-		student_array
 	end
-
+	student_array
 end
 
